8e3e48b43b59633a6989b7ae12b97ce6897811ab
wzgeda <wzgeda@qq.com>
2022-12-29 16:01:18 +0800

    Update trojan-go-script.sh

--- trojan-go-script.sh
+++ trojan-go-script.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
 function install(){
-    echo 开启防火墙并创建开机启动服务
+    # 开启防火墙并创建开机启动服务
     systemctl enable --now firewalld
 
-    echo 关闭22端口，关闭sshd服务
+    # 关闭22端口，关闭sshd服务
     firewall-cmd --permanent --remove-service=ssh
     firewall-cmd --reload
     systemctl stop sshd
     systemctl disable sshd
 
-    echo 重建软件仓库缓存索引
+    # 重建软件仓库缓存索引
     yum makecache
 
-    echo 安装certbot nginx unzip
+    # 安装certbot nginx unzip
     yum install certbot python2-certbot-nginx nginx unzip -y
 
-    echo 创建nginx开机启动
+    # 创建nginx开机启动
     systemctl enable nginx
 
     if [ ! -f /etc/nginx/nginx.conf.bak ];then
-        echo nginx.conf备份不存在备份生成nginx.conf.bak
+        # nginx.conf备份不存在,备份生成nginx.conf.bak
         cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
     fi
-    read -p "请输入想要认证的域名: " sni
-    echo 为认证修改nginx.conf配置
+    read -p "Please enter the domain name you want to authenticate: " sni
+    # 为认证修改nginx.conf配置
     sed -i "s/server_name.*_;/server_name\t${sni};/" /etc/nginx/nginx.conf
 
-    echo 重启nginx服务
+    # 重启nginx服务
     systemctl restart nginx
 
-    echo 删除默认html网页新建一个index.html
+    # 删除默认html网页新建一个index.html
     rm -rf /usr/share/nginx/html/*
     echo "not" > /usr/share/nginx/html/index.html
 
-    echo 为认证临时开启http防火墙
+    # 为认证临时开启http防火墙
     firewall-cmd --add-service=http
 
-    echo 开始认证
+    # 开始认证
     certbot --nginx -d ${sni}
 
-    echo 设置证书与key文件地址变量ca与key
+    # 设置证书与key文件地址变量ca与key
     ca=/etc/letsencrypt/live/${sni}/fullchain.pem
     key=/etc/letsencrypt/live/${sni}/privkey.pem
 
-    echo 恢复因为认证被修改的nginx.conf
+    # 恢复因为认证被修改的nginx.conf
     \cp /etc/nginx/nginx.conf.bak /etc/nginx/nginx.conf
 
     # echo nginx只监听本地网络
@@ -52,24 +52,26 @@ function install(){
     echo 重启nginx服务
     systemctl restart nginx
 
-    echo 定义变量,获取版本号与最新trojan-go下载地址,下载并解压
+    # 定义变量,获取版本号与最新trojan-go下载地址,下载并解压
     base_url=https://github.com/p4gefau1t/trojan-go
     version=$(curl ${base_url} | grep 'style="max-width: none' | sed -r 's|.*>(v.*)<.*|\1|')
     download_url=$base_url/releases/download/${version}/trojan-go-linux-amd64.zip
-    wget ${download_url}
+    wget ${download_url} -O trojan-go-linux-amd64.zip
     unzip -o trojan-go-linux-amd64.zip -d ./trojan-go
-    echo 拷贝trojan-go到bin
+    # 拷贝trojan-go到bin
     \cp ./trojan-go/trojan-go /usr/bin/
     if [ ! -d "/etc/trojan-go/" ];then
-        echo 创建trojan-go配置文件
+        # 创建trojan-go配置目录
         mkdir /etc/trojan-go/
     fi
-    if [ ! -f "/etc/trojan-go/server.json" ];then
+    if [ -f "/etc/trojan-go/server.json" ];then
+        # 如果存在server.json 删除
         rm -f /etc/trojan-go/server.json  
     fi
 
-    echo 创建server.json文件
-    read -p "请输入端口号,密码,websocket路径: " port password path
+    # 创建server.json文件
+    read -p "Please enter the trojan-go port number: " port
+    read -p "Please enter the trojan-go password: " password
     cat > /etc/trojan-go/server.json <<-EOF
     {
         "run_type": "server",
@@ -84,15 +86,6 @@ function install(){
             "cert": "${ca}",
             "key": "${key}",
             "sni": "${sni}"
-        },
-        "websocket": {
-            "enabled": true,
-            "path": "${path}",
-            "host": "${sni}"
-        },
-        "shadowsocks": {
-            "enabled": true,
-            "password": "${password}"
         }
     }
 EOF
@@ -100,7 +93,7 @@ EOF
     if [ ! -f /etc/systemd/system/trojan-go.service ];then
         sudo rm -f /etc/systemd/system/trojan-go.service
     fi
-    echo 创建trojan-go服务
+    # 创建trojan-go服务
     cat > /etc/systemd/system/trojan-go.service <<-EOF
     [Unit]
     Description=Trojan-Go - An unidentifiable mechanism that helps you bypass GFW
@@ -120,19 +113,39 @@ EOF
     [Install]
     WantedBy=multi-user.target
 EOF
-    echo 配置续期证书定时任务每天执行certbot renew
+    # 配置续期证书定时任务每天执行certbot renew
     \cp ./trojan-go-script.sh /opt/
     echo "0 2 * * * /opt/trojan-go-script.sh renew" > /var/spool/cron/root
     
-    echo 开启trojan-go防火墙端口
+    # 开启trojan-go防火墙端口
     firewall-cmd --permanent --add-port=${port}/tcp
     firewall-cmd --reload
-    echo 重新载入systemctl配置文件
+    # 重新载入systemctl配置文件
     systemctl daemon-reload
-    echo 启动trojan-go服务
+    # 启动trojan-go服务
     systemctl restart trojan-go
-    echo 配置开机启动服务
+    # 配置开机启动服务
     systemctl enable trojan-go
+    # 生成客户端配置文件
+    cat > client.json <<-EOF
+    {
+        "run_type": "client",
+        "local_addr": "127.0.0.1",
+        "local_port": 1080,
+        "remote_addr": "${sni}",
+        "remote_port": ${port},
+        "password": [
+            "${password}"
+        ],
+        "ssl": {
+            "sni": "${sni}"
+        },
+        "mux": {
+            "enabled": true
+        }
+    }
+EOF
+    # 显示当前状态
     systemctl status trojan-go
 }
 
